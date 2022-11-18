85d2d5a600c1438d4fe3adf0f9f401c748ee2437
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-11-18 16:55:35 -0300

    18/11 - 16:55
    Continuação da autenticação

--- src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
+++ src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
@@ -1,8 +1,10 @@
 package br.senai.sc.editoralivros.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,5 +45,12 @@ public class AutenticacaoConfig extends WebSecurityConfigurerAdapter {
         authenticationManagerBuilder
                 .userDetailsService(autenticacaoService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    // Utilizado para realizar a autenticação em AutenticacaoController
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
--- src/main/java/br/senai/sc/editoralivros/security/AutenticacaoService.java
+++ src/main/java/br/senai/sc/editoralivros/security/AutenticacaoService.java
@@ -2,18 +2,22 @@ package br.senai.sc.editoralivros.security;
 
 import br.senai.sc.editoralivros.model.entities.Pessoa;
 import br.senai.sc.editoralivros.repository.PessoaRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
+
+    private final String senhaForte = "c127a7b6adb013a5ff879ae71afa62afa4b4ceb72afaa54711dbcde67b6dc325";
 
     @Autowired
     private PessoaRepository pessoaRepository;
@@ -21,9 +25,37 @@ public class AutenticacaoService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Pessoa> pessoaOptional = pessoaRepository.findByEmail(username);
-        if(pessoaOptional.isPresent()) {
+        if (pessoaOptional.isPresent()) {
             return pessoaOptional.get();
         }
         throw new UsernameNotFoundException("Usuário não encontrado!");
+    }
+
+    public String gerarToken(Authentication authentication) {
+        Pessoa pessoa = (Pessoa) authentication.getPrincipal();
+        return Jwts.builder()
+                .setIssuer("Editora de Livros")
+                .setSubject(pessoa.getCpf().toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 18000000))
+                .signWith(SignatureAlgorithm.HS256, senhaForte)
+                .compact();
+    }
+
+    public Boolean validarToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(senhaForte).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Pessoa getUsuario(String token) {
+        Long cpf = Long.parseLong(Jwts.parser()
+                .setSigningKey(senhaForte)
+                .parseClaimsJws(token)
+                .getBody().getSubject());
+        return pessoaRepository.findById(cpf).get();
     }
 }
