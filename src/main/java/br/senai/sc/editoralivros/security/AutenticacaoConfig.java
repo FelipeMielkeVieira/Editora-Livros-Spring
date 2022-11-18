9febd547b18f6fa732cbc85865be1f2c1bbcba9e
Felipe Vieira <felipemielkevieira2@gmail.com>
2022-11-18 16:59:26 -0300

    a
    a

--- src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
+++ src/main/java/br/senai/sc/editoralivros/security/AutenticacaoConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -32,7 +33,8 @@ public class AutenticacaoConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
 //                    .and().formLogin()
                     .and().csrf().disable()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and().addFilterBefore(new AutenticacaoFiltro(autenticacaoService), UsernamePasswordAuthenticationFilter.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
